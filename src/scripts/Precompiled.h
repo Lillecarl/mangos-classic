9addf87b835f68fb7f433c1e157f368cab92eee0
LilleCarl <lillecarl@gmail.com>
2014-09-03 07:20:00 +0200

    New scripting system in!

---
+++ src/scripts/Precompiled.h
@@ -0,0 +1,9 @@
+#ifndef SCRIPTPCH_H
+#define SCRIPTPCH_H
+
+#include "pchdef.h"
+#include "Custom.h"
+#include "GossipDef.h"
+#include "CPlusMgr.h"
+
+#endif
---
+++ src/scripts/npc_training_dummy.cpp
@@ -0,0 +1,87 @@
+/*
+* See AUTHORS file for Copyright information
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "Precompiled.h"
+#include "CreatureAI.h"
+#include "CPlusMgr.h"
+
+class npc_training_dummyAI : public CreatureAI
+{
+public:
+    npc_training_dummyAI(Creature* pCreature) : CreatureAI(pCreature){}
+
+    typedef UNORDERED_MAP<ObjectGuid, uint32> AttackerMap;
+    AttackerMap m_AttackerMap;
+
+    void Reset() {}
+
+    void DamageTaken(Unit* pDealer, uint32& uiDamage)
+    {
+        if (pDealer->GetTypeId() == TYPEID_PLAYER)
+            m_AttackerMap[pDealer->GetObjectGuid()] = 5000;
+
+        m_creature->SetHealth(m_creature->GetMaxHealth() * 0.2f);
+        //m_creature->SetTargetGuid(m_creature->GetObjectGuid());
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        for (AttackerMap::iterator itr = m_AttackerMap.begin(); itr != m_AttackerMap.end();)
+        {
+            if (itr->second < diff)
+            {
+                if (Player* pAttacker = m_creature->GetMap()->GetPlayer(itr->first))
+                {
+                    pAttacker->CombatStop(true);
+                    pAttacker->AttackStop(false);
+                    pAttacker->CombatStopWithPets(true);
+                    pAttacker->ClearInCombat();
+                }
+
+                itr = m_AttackerMap.erase(itr);
+
+                if (m_AttackerMap.empty())
+                {
+                    EnterEvadeMode();
+                    m_creature->SetHealth(m_creature->GetMaxHealth());
+                }
+            }
+            else
+            {
+                itr->second -= diff;
+                ++itr;
+            }
+        }
+    }
+};
+
+class npc_training_dummy : public CreatureScript
+{
+public:
+    npc_training_dummy() : CreatureScript("npc_training_dummy") {}
+
+    CreatureAI* GetCreatureAI(Creature* pCreature) override
+    {
+        return new npc_training_dummyAI(pCreature);
+    }
+};
+
+void AddSC_npc_training_dummy()
+{
+    new npc_training_dummy;
+}
